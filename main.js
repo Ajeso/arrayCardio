91383feaa29c32b0fa5b03b206caade29163779d
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2026-02-12 21:42:58 +0300

    Used the reduce  and sort array methods

--- main.js
+++ main.js
@@ -79,13 +79,44 @@ console.table(fullNames);
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 
+// const ordered = inventors.sort(function (a, b) {
+//   if (a.year > b.year) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+
+const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+console.table(ordered);
+
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYears);
+
 // 5. Sort the inventors by years lived
+
+const oldest = inventors.sort((a, b) => {
+  const lastPerson = a.passed - a.year;
+  const nextPerson = b.passed - b.year;
+
+  return lastPerson > nextPerson ? -1 : 1;
+});
+console.table(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+const category = document.querySelector(".mw-category ul li a");
+console.log(category);
+const links = Array.from(category);
+
+const de = links.map((link) => link.textContent);
+console.log(de);
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
